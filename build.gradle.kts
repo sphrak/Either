ffa6d6c7142fbd9cb80ffc178a1b38957afb7193
Niclas <niclas.kroon@protonmail.ch>
2020-02-11 20:41:22 +0100

    fixes #2 -- bump kotlin 1.3.61

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:3.6.0-alpha08")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.50")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.61")
         classpath("com.github.jengelman.gradle.plugins:shadow:5.0.0")
         classpath("com.jfrog.bintray.gradle:gradle-bintray-plugin:1.8.4")
         // NOTE: Do not place your application dependencies here; they belong
@@ -18,7 +18,7 @@ allprojects {
         jcenter()
         mavenCentral()
     }
-    version = "1.0.1"
+    version = "1.0.2"
     group = "io.github.sphrak"
 }
 
